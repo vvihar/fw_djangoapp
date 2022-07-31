fa10140b7c3b17ae1e96ee6d192fa38ee1567f29
Gulmohar <92192914+vvihar@users.noreply.github.com>
2022-07-31 23:47:31 +0900

    セクション13 modelからつくられるobject

--- boardapp/views.py
+++ boardapp/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 
 def signupfunc(request):
+    object = User.objects.get(username='yakushijin-s')  # すべての情報を格納
+    print(object.email)
     if request.method == 'POST':
         print('this is POST')
     else:
