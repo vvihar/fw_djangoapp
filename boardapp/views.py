689b6543d8701f5fa404f5215d35a5b6d343a8b0
Gulmohar <92192914+vvihar@users.noreply.github.com>
2022-07-31 22:58:18 +0900

    セクション13 POSTとGET

--- boardapp/views.py
+++ boardapp/views.py
@@ -4,5 +4,9 @@ from django.shortcuts import render
 
 
 def signupfunc(request):
+    if request.method == 'POST':
+        print('this is POST')
+    else:
+        print('this is GET')
     # request, テンプレのファイル, モデルのデータを引数にとる
     return render(request, 'board/signup.html', {'some': 100})
