358d1590cbd76cd568fe27d01a9039986c30bc96
Gulmohar <92192914+vvihar@users.noreply.github.com>
2022-08-01 01:03:10 +0900

    セクション13 loginview

--- boardapp/urls.py
+++ boardapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import signupfunc
+from .views import signupfunc, loginfunc
 
 urlpatterns = [
     path('signup/', signupfunc),
+    path('login/', loginfunc),
 ]
--- boardapp/views.py
+++ boardapp/views.py
@@ -1,5 +1,8 @@
+from sqlite3 import IntegrityError
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
@@ -7,8 +10,29 @@ from django.contrib.auth.models import User
 def signupfunc(request):
     # print(request.POST)
     if request.method == 'POST':
-        username = request.POST['username']
+        username = request.POST['username']  # name属性を使用
         password = request.POST['password']
-        user = User.objects.create_user(username, '', password)
+        try:
+            user = User.objects.create_user(username, '', password)
+            return render(request, 'board/signup.html', {})
+        except IntegrityError:  # 重複したユーザー名の場合
+            return render(request, 'board/signup.html', {'error': 'このユーザー名は既に使用されています'})
     # request, テンプレのファイル, モデルのデータを引数にとる
-    return render(request, 'board/signup.html', {'some': 100})
+    return render(request, 'board/signup.html', {})
+
+
+def loginfunc(request):
+    if request.method == 'POST':
+        if request.method == 'POST':
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:  # user がいる場合
+                login(request, user)
+                # login 成功
+                return render(request, 'board/login.html', {'context': 'logged in'})
+            else:
+                # login 失敗
+                return render(request, 'board/login.html', {'context': 'not logged in'})
+    # ただのアクセス
+    return render(request, 'board/login.html', {'context': 'get method'})
