c3fcddce16cf228ccebeaf5f0380c599e44e5cc2
Gulmohar <92192914+vvihar@users.noreply.github.com>
2022-08-16 01:19:11 +0900

    Debugモードの切り替え設定

--- fwappproject/settings.py
+++ fwappproject/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from django.contrib import messages
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,9 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nlzgroibgczh&tw$d0^%%#7s*l@+-um1qu@3@&0a46y4ir^m44'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+DEBUG = False
 
 
 # Application definition
@@ -153,4 +150,11 @@ MESSAGE_TAGS = {
 FORM_RENDERER = 'django.forms.renderers.TemplatesSetting'
 
 # 最後に書く
-django_heroku.settings(locals())
+try:
+    from .local_settings import *
+except ImportError:
+    pass
+
+if not DEBUG:
+    import django_heroku
+    django_heroku.settings(locals())
